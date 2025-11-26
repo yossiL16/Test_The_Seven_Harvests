56bf7139807dbe2e3b0c2ff25ae95e80a018f0e9
yossi lerer <ylerer770@gmail.com>
2025-11-26 11:08:02 +0200

    add solider class

---
+++ object/soldier.py
@@ -0,0 +1,13 @@
+
+
+
+class Soldier:
+
+    def __init__(self,personal_number:int, f_name:str, l_name:str, gender:str,city:str, distance:int):
+        self.personal_number = personal_number
+        self.f_name = f_name
+        self.l_name = l_name
+        self.gender = gender
+        self.city = city
+        self.distance = distance
+        self.status = None
